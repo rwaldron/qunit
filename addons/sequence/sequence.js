7cd919b939c9e23f95c48e8a8257fb7c3571f87e
John Bender <john.m.bender@gmail.com>
2012-03-01 12:16:29 -0800

    event and deferred sequence helpers and minimal tests

---
+++ addons/sequence/sequence.js
@@ -0,0 +1,67 @@
+QUnit.extend( QUnit, {
+	eventCascade: function( sequence, timedOut, eventInfo ) {
+		var fn = sequence.shift(),
+			event = sequence.shift(),
+			self = this;
+
+		if( fn === undefined ) {
+			return;
+		}
+
+		if( event ){
+			// if a pagechange or defined event is never triggered
+			// continue in the sequence to alert possible failures
+			var warnTimer = setTimeout(function() {
+				self.eventCascade( sequence, true );
+			}, 2000);
+
+
+			function listener( eventInfo ) {
+				clearTimeout( warnTimer );
+
+				// we only want this listener to get fired once so unbind
+				document.removeEventListener( event, listener );
+
+				// Let the current stack unwind before we fire off the next item in the sequence.
+				// TODO setTimeout(self.pageSequence, 0, sequence);
+				setTimeout(function(){ self.eventCascade(sequence); }, 0);
+			};
+
+			// bind the recursive call to the event
+			document.addEventListener(event, listener);
+		}
+
+		// invoke the function which should, in some fashion,
+		// trigger the next event
+		fn( timedOut );
+	},
+
+	deferredSequence: function(fns) {
+		if( !window.jQuery || !window.jQuery.Deferred ){
+			throw "jQuery and jQuery.Deferred must be defined to use deferredSequence";
+		}
+
+		var fn = fns.shift(),
+			deferred = window.jQuery.Deferred(),
+			self = this, res;
+
+		if (fn) {
+			res = fn();
+			if ( res && $.type( res.done ) === "function" ) {
+				res.done(function() {
+					self.deferredSequence( fns ).done(function() {
+						deferred.resolve();
+					});
+				});
+			} else {
+				self.deferredSequence( fns ).done(function() {
+					deferred.resolve();
+				});
+			}
+		} else {
+			deferred.resolve();
+		}
+
+		return deferred;
+	}
+});
---
+++ addons/sequence/sequenceTest.js
@@ -0,0 +1,54 @@
+asyncTest( "eventCascade", function() {
+	expect( 2 );
+
+	function triggerMouseEventOn( id ){
+			var event = document.createEvent( "MouseEvent" );
+			event.initMouseEvent( "click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+			document.getElementById( id ).dispatchEvent( event );
+	}
+
+	QUnit.eventCascade([
+		function() {
+			triggerMouseEventOn( "foo" );
+		},
+
+		"click", function( timedOut ) {
+			ok( !timedOut, "the event wasn't run as a timeout" );
+
+			triggerMouseEventOn( "bar" );
+		},
+
+		"click", function( timedOut ) {
+			ok( !timedOut, "the event wasn't run as a timeout" );
+
+			start();
+		}
+	]);
+});
+
+asyncTest( "deferredSequence", function() {
+	expect( 2 );
+
+	QUnit.deferredSequence([
+		function() {
+			var deferred = jQuery.Deferred();
+			deferred.resolve();
+			return deferred;
+		},
+
+		function() {
+			ok( true );
+		},
+
+		function() {
+			var deferred = jQuery.Deferred();
+			deferred.resolve();
+			return deferred;
+		},
+
+		function() {
+			ok( true );
+			start();
+		}
+	]);
+});
