b3512a9dce7bdbe3b1a2431f8bfa75a884bc6e63
Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>
2012-03-12 17:18:18 -0400

    API re-org
    Signed-off-by: Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>

--- addons/sequence/sequence.js
+++ addons/sequence/sequence.js
@@ -1,67 +1,75 @@
 QUnit.extend( QUnit, {
-	// TODO would be nice to use jquery events where possible
-	//      and to forward the even object for testing if possible
-	eventSequence: function( sequence, timedOut, eventInfo ) {
-		var fn = sequence.shift(),
-			event = sequence.shift(),
-			self = this;
+  sequence: {
+    events: function( sequence, timedOut, eventInfo ) {
+      var fn = sequence.shift(),
+          event = sequence.shift(),
+          self = this,
+          warnTimer;
 
-		// if the event isn't defined we've reached the end of the sequence.
-		// fire the shifted fn at the bottom to finish the sequence
-		if( event ){
-			// if the event is never triggered continue in the sequence
-			// to alert the user to possible failures
-			var warnTimer = setTimeout(function() {
-				self.eventSequence( sequence, true );
-			}, 2000);
+      // `
+      if ( fn === undefined ) {
+        return;
+      }
+      // if the event isn't defined we've reached the end of the sequence.
+      // fire the shifted fn at the bottom to finish the sequence
+
+      if ( event ) {
+        // if a pagechange or defined event is never triggered
+        // continue in the sequence to alert possible failures
+        warnTimer = setTimeout(function() {
+          QUnit.sequence.events( sequence, true );
+        }, 2000);
 
 
-			function listener( eventInfo ) {
-				clearTimeout( warnTimer );
+        function listener( eventInfo ) {
+          clearTimeout( warnTimer );
 
-				// we only want this listener to get fired once so unbind
-				document.removeEventListener( event, listener );
+          // we only want this listener to get fired once so unbind
+          document.removeEventListener( event, listener );
 
-				// Let the current stack unwind before we fire off the next item in the sequence.
-				// TODO setTimeout(self.pageSequence, 0, sequence);
-				setTimeout(function(){ self.eventSequence(sequence); }, 0);
-			};
+          // Let the current stack unwind before we fire off the next item in the sequence.
+          // TODO setTimeout(self.pageSequence, 0, sequence);
+          setTimeout(function() {
+            QUnit.sequence.events( sequence );
+          }, 0);
+        };
 
-			// bind the recursive call to the event
-			document.addEventListener(event, listener);
-		}
+        document.addEventListener( event, listener, false );
+      }
+      // invoke the function which should either trigger the next event
+      // or finish out the sequence if no event was defined
+      fn( timedOut );
+    },
 
-		// invoke the function which should either trigger the next event
-		// or finish out the sequence if no event was defined
-		fn( timedOut );
-	},
+    deferred: function( fns ) {
+      if ( !window.jQuery || !window.jQuery.Deferred ) {
+        throw "jQuery and jQuery.Deferred must be defined to use QUnit.sequence.deferred";
+      }
 
-	deferredSequence: function(fns) {
-		if( !window.jQuery || !window.jQuery.Deferred ){
-			throw "jQuery and jQuery.Deferred must be defined to use deferredSequence";
-		}
+      var fn = fns.shift(),
+          deferred = window.jQuery.Deferred(),
+          resolved;
 
-		var fn = fns.shift(),
-			deferred = window.jQuery.Deferred(),
-			self = this, res;
+      if (fn) {
 
-		if (fn) {
-			res = fn();
-			if ( res && $.type( res.done ) === "function" ) {
-				res.done(function() {
-					self.deferredSequence( fns ).done(function() {
-						deferred.resolve();
-					});
-				});
-			} else {
-				self.deferredSequence( fns ).done(function() {
-					deferred.resolve();
-				});
-			}
-		} else {
-			deferred.resolve();
-		}
+        resolved = fn();
 
-		return deferred;
-	}
+        if ( resolved && jQuery.isFunction(resolved.done) ) {
+          resolved.done(function() {
+            QUnit.sequence.deferred( fns ).done(function() {
+              deferred.resolve();
+            });
+          });
+        } else {
+          QUnit.sequence.deferred( fns ).done(function() {
+            deferred.resolve();
+          });
+        }
+      } else {
+        deferred.resolve();
+      }
+
+      return deferred;
+    }
+  }
 });
--- addons/sequence/sequenceTest.js
+++ addons/sequence/sequenceTest.js
@@ -1,15 +1,15 @@
-asyncTest( "eventCascade", function() {
+asyncTest( "sequence.events", function() {
 	expect( 3 );
 
-	function triggerMouseEventOn( id ){
-		var event = document.createEvent( "MouseEvent" );
+	function triggerMouseEventOn( id ) {
+		var event = document.createEvent("MouseEvent");
 		event.initMouseEvent( "click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
 		document.getElementById( id ).dispatchEvent( event );
 	}
 
-	QUnit.eventSequence([
+	QUnit.sequence.events([
 		function() {
-			triggerMouseEventOn( "foo" );
+			triggerMouseEventOn("foo");
 		},
 
 		"click", function( timedOut ) {
@@ -29,10 +29,10 @@ asyncTest( "eventCascade", function() {
 	]);
 });
 
-asyncTest( "deferredSequence", function() {
+asyncTest( "sequence.deferred", function() {
 	expect( 2 );
 
-	QUnit.deferredSequence([
+  QUnit.sequence.deferred([
 		function() {
 			var deferred = jQuery.Deferred();
 			deferred.resolve();
